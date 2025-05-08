6b28c333fadf10f61db42afddafa988eeedbcab7
helldiverCode <37001689+helldiverCode@users.noreply.github.com>
2025-05-08 12:13:00 +0200

    add tests

--- src/test/java/com/example/bgr/service/impl/GameServiceImplTest.java
+++ src/test/java/com/example/bgr/service/impl/GameServiceImplTest.java
@@ -16,12 +16,13 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -78,5 +79,93 @@ class GameServiceImplTest {
         assertEquals(2, result.size());
         verify(gameDao, times(1)).findAll(any(Specification.class), any(Pageable.class));
         verify(gameMapper, times(1)).mapToDtos(gameEntities);
+    }
+
+    @Test
+    void testGetGameById() {
+        // Arrange
+        Long id = 1L;
+        GameEntity gameEntity = new GameEntity();
+        GameDTO gameDTO = new GameDTO();
+
+        when(gameDao.findById(any(Long.class))).thenReturn(Optional.of(gameEntity));
+        when(gameMapper.mapToDto(gameEntity)).thenReturn(gameDTO);
+
+        // Acr
+        GameDTO result = gameService.getGameById(id);
+
+        //Assert
+        assertNotNull(result);
+        verify(gameDao, times(1)).findById(id);
+        verify(gameMapper, times(1)).mapToDto(gameEntity);
+    }
+
+    @Test
+    void testGetGameById_shouldThrowException() {
+        // Arrange
+        Long id = 1L;
+
+        when(gameDao.findById(any(Long.class))).thenReturn(Optional.empty());
+
+        // Acr
+        Exception exception = assertThrows(ResponseStatusException.class, () -> {
+            gameService.getGameById(id);
+        });
+
+
+        // Assert
+        String expectedMessage = "Game not found with ID: " + id;
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+
+        verify(gameDao, times(1)).findById(id);
+        verify(gameMapper, never()).mapToDto(any());
+    }
+
+    @Test
+    void saveGame_shouldSaveSuccessfully_whenGameIsValidAndNotExists() {
+        // Arrange
+        GameDTO gameDTO = new GameDTO();
+        gameDTO.setId(null);
+        gameDTO.setBarcode("12345");
+
+        GameEntity gameEntity = new GameEntity();
+        GameEntity savedEntity = new GameEntity();
+        GameDTO savedDTO = new GameDTO();
+
+        when(gameDao.existsByBarcode("12345")).thenReturn(false);
+        when(gameMapper.mapToEntity(gameDTO)).thenReturn(gameEntity);
+        when(gameDao.save(gameEntity)).thenReturn(savedEntity);
+        when(gameMapper.mapToDto(savedEntity)).thenReturn(savedDTO);
+
+        // Act
+        GameDTO result = gameService.saveGame(gameDTO);
+
+        // Assert
+        assertNotNull(result);
+        verify(gameDao).existsByBarcode("12345");
+        verify(gameDao).save(gameEntity);
+        verify(gameMapper).mapToEntity(gameDTO);
+        verify(gameMapper).mapToDto(savedEntity);
+    }
+
+    @Test
+    void saveGame_shouldThrowException_whenGameWithSameBarcodeExists() {
+        // Arrange
+        GameDTO gameDTO = new GameDTO();
+        gameDTO.setId(null);
+        gameDTO.setBarcode("12345");
+
+        when(gameDao.existsByBarcode("12345")).thenReturn(true);
+
+        // Act & Assert
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class,
+                () -> gameService.saveGame(gameDTO));
+
+        assertEquals("400 BAD_REQUEST \"A game with the same barcode already exists.\"", exception.getMessage());
+        verify(gameDao).existsByBarcode("12345");
+        verify(gameMapper, never()).mapToEntity(any());
+        verify(gameDao, never()).save(any());
     }
 }
